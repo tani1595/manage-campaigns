d82fbd99382294976382226a988de79d6341eff8
Tania Sharma <32297902+tani1595@users.noreply.github.com>
2020-05-21 16:41:18 +0530

    Updates

---
+++ precache-manifest.33fa9d43799fca2aae2256d61df9ce06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9060d4c9b5666686c5915fe91a4f3010",
+    "url": "/manage-campaigns/index.html"
+  },
+  {
+    "revision": "fa2c862025cc8673f7cb",
+    "url": "/manage-campaigns/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "4a7973fb8de700d4f098",
+    "url": "/manage-campaigns/static/css/main.270b1163.chunk.css"
+  },
+  {
+    "revision": "fa2c862025cc8673f7cb",
+    "url": "/manage-campaigns/static/js/2.cbe0afb1.chunk.js"
+  },
+  {
+    "revision": "0808c75b50af43a39fe893124e2f5e72",
+    "url": "/manage-campaigns/static/js/2.cbe0afb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a7973fb8de700d4f098",
+    "url": "/manage-campaigns/static/js/main.7bc4fd78.chunk.js"
+  },
+  {
+    "revision": "d627489aa428d17a344d",
+    "url": "/manage-campaigns/static/js/runtime-main.1ca3fff0.js"
+  },
+  {
+    "revision": "49c3ed292b144f9a8aef4a1967184776",
+    "url": "/manage-campaigns/static/media/logo.49c3ed29.png"
+  }
+]);
